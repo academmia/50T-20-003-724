6d8703e04d9df6ba873a0183fa24509196e1f786
Academmia <academmia.com@gmail.com>
2020-11-02 02:08:11 +0200

    'init'

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+ 
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Academmia</h1>
+      <h2>useEffect() hook</h2>
+    </div>
+  );
+}
